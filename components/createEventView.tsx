edac66a6ee2fa33cca7d6615c69dec5bb8425264
Adrian Hossen <adrianhossen5@gmail.com>
2023-03-28 14:29:38 -0400

    More Changes

--- components/createEventView.tsx
+++ components/createEventView.tsx
@@ -1,17 +1,37 @@
 import { useSession } from 'next-auth/react';
 import React, { useState } from 'react';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDateFns } from '@mui/x-date-pickers-pro/AdapterDateFns';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const EventsCreateView = () => {
   const [title, setTitle] = useState('');
+  const [date, setDate] = useState<Date | null>(null);
+  const [type, setType] = useState('PUBLIC');
   const [body, setBody] = useState('');
-  
-  const submitEvent = async (note: {
-    title: string;
-    body: string;
+
+  const fontFamily = 'system-ui';
+  const theme = createTheme({
+    components: {
+      MuiOutlinedInput: {
+        styleOverrides: {
+          root: {
+            fontFamily,
+          },
+        },
+      },
+    },
+  });
+
+  const submitEvent = async (event: {
+    title: string | undefined | null;
+    body: string | undefined | null;
+    type: string | undefined | null;
+    date: Date | undefined | null;
   }) => {
     const response = await fetch('/api/eventCreate', {
       method: 'POST',
-      body: JSON.stringify(note),
+      body: JSON.stringify(event),
     });
 
     const data = await response.json();
@@ -22,14 +42,24 @@ const EventsCreateView = () => {
     return new Promise((res) => setTimeout(res, delay));
   };
 
+  const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    let value = event.target.value;
+    setType(value)
+
+    return (<div>{value}</div>);
+  };
+
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    const event = { title, body };
+
+    const event = { title, body, type, date };
     await submitEvent(event);
     await timeout(1000);
     window.location.reload();
     setBody('');
     setTitle('');
+    setType('PUBLIC');
+    setDate(null);
   };
 
   return (
@@ -39,12 +69,12 @@ const EventsCreateView = () => {
           className="mx-auto max-w-md text-xl text-left xs:max-w-md sm:max-w-md 
         md:max-w-md lg:max-w-md xl:max-w-md 2xl:max-w-md"
         >
-          <div className="mb-4 text-lg font-bold">
+          <div className="mb-4 text-lg">
             <div
               className="rounded-[0.175rem] w-max border-l-[0.175rem] border-t-[0.175rem] border-r-[0.175rem] 
                 border-neutral-700 px-2 font-bold transition bg-neutral-300 text-lg"
             >
-              Event Title:
+              Title:
             </div>
             <textarea
               maxLength={30}
@@ -53,19 +83,33 @@ const EventsCreateView = () => {
               required
               rows={1}
               cols={1}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-neutral-50 rounded-lg border-[0.175rem] 
-              rounded-tl-none border-neutral-700"
+              className="block p-2.5 w-full text-md text-gray-900 bg-neutral-50 rounded-lg border-[0.175rem] 
+              rounded-tl-none border-neutral-700 "
             ></textarea>{' '}
           </div>
-          <div className="mb-4 text-lg font-bold">
+          <div className="mb-4 text-lg">
             <div
               className="rounded-[0.175rem] w-max border-l-[0.175rem] border-t-[0.175rem] border-r-[0.175rem] 
                 border-neutral-700 px-2 font-bold transition bg-neutral-300 text-lg"
             >
-              Date
+              Date:
+            </div>
+            <div
+              className="block w-full text-sm text-gray-900 bg-neutral-50 rounded-lg border-[0.175rem] 
+                rounded-tl-none border-neutral-700"
+            >
+              <ThemeProvider theme={theme}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                    className="w-full"
+                    value={date}
+                    onChange={(newValue) => setDate(newValue)}
+                  />
+                </LocalizationProvider>
+              </ThemeProvider>
             </div>
           </div>
-          <div className="mb-4 text-lg font-bold">
+          <div className="mb-4 text-lg">
             <div
               className="rounded-[0.175rem] w-max border-l-[0.175rem] border-t-[0.175rem] border-r-[0.175rem] 
                 border-neutral-700 px-2 font-bold transition bg-neutral-300 text-lg"
@@ -80,22 +124,46 @@ const EventsCreateView = () => {
                 required
                 rows={7}
                 cols={1}
-                className="block p-2.5 w-full text-sm text-gray-900 bg-neutral-50 rounded-lg border-[0.175rem] 
+                className="block p-2.5 w-full text-md text-gray-900 bg-neutral-50 rounded-lg border-[0.175rem] 
                 rounded-tl-none border-neutral-700"
               ></textarea>{' '}
             </div>
           </div>
-          <div className="mb-4 text-lg font-bold">
+          <div className="mb-4 text-lg">
             <div
               className="rounded-[0.175rem] w-max border-l-[0.175rem] border-t-[0.175rem] border-r-[0.175rem] 
                 border-neutral-700 px-2 font-bold transition bg-neutral-300 text-lg"
             >
-              Event Type / Uni
+              Event Type:
             </div>
-            <div></div>
+            <div
+              className="flex flex-col p-2.5 w-28 text-md text-gray-900 bg-neutral-50 rounded-lg border-[0.175rem] 
+                rounded-tl-none border-neutral-700"
+            >
+              <select
+                name="type"
+                required
+                defaultValue="PUBLIC"
+                onChange={selectChange}
+              >
+                <option
+                  value="PUBLIC"
+                >
+                  Public
+                </option>
+                <option
+                  value="PRIVATE"
+                >
+                  Private
+                </option>
+                <option value="RSO">
+                  RSO
+                </option>
+              </select>
+            </div>
           </div>
         </div>
-        <div className="py-[14px]">
+        <div className="py-[32px]">
           <button>
             <div
               className="mx-auto rounded-[0.5rem] w-max border-[0.175rem] border-neutral-700 px-3 py-1 font-bold transition
--- pages/api/eventCreate.ts
+++ pages/api/eventCreate.ts
@@ -1,23 +1,36 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { type } from 'os';
+import { stringify } from 'querystring';
 import prisma from '../../lib/prismadb';
+
+const EventType = {
+  PUBLIC: 'PUBLIC',
+  PRIVATE: 'PRIVATE',
+  RSO_EVENT: 'RSO_EVENT',
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
-    const {body, title} = JSON.parse(req.body);
-
     try {
+      const {title, body, type, date} = JSON.parse(req.body);
+
+      console.log("Hi this is event" + type[0]);
+
+
       const eventCreation = await prisma.event.create({
         data: {
           name: title,
-          // date: newDate,
-        },
-            });
+          type: type,
+          description: body,
+          date: date
+        }
+      });
 
     } catch (error) {
-      
+      console.log(error)
     }
 
     res.status(201).json(null)
