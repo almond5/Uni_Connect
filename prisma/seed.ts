40faba3ae9cf1525e6573ce708348b7e869b5b95
Adrian Hossen <adrianhossen5@gmail.com>
2023-04-14 20:54:10 -0400

    Seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: 'Testing',
+      password: 'asdsada',
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
